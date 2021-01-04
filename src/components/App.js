822897ee0db1cec2aec430290c368a32fa3dd331
eduardisrael <israel_pasaca@icloud.com>
2021-01-03 20:20:25 -0500

    PageNotFound, courses component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from './common/Header';
+import PageNotFound from "./PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
 
 function App(){
     return (
         <div className="container-fluid">
             <Header />
-            <Route exact path="/" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/courses" component={CoursesPage} />
+                <Route component={PageNotFound}/>
+            </Switch>
         </div>
     );
 }
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,7 +7,9 @@ const Header = () => {
     return(
         <nav>
             <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>{" | "}
+            <NavLink to="/courses" activeStyle={activeStyle}>Courses</NavLink>{" | "}
             <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
+
         </nav>
     )
 }
