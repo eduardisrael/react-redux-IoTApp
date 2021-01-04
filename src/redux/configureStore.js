254c24c0ed20afb131bb4934d286f66090abf0b7
eduardisrael <israel_pasaca@icloud.com>
2021-01-04 03:57:46 -0500

    CreateStore Redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { rootReducer } from "./reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+
+/*es util tener una funcion que configura la tienda. Punto de entrada de nuestra app 
+Redux middleware is a way to enhance redux's behavior, with extra functionality. 
+reduxImmutableStateInvariant: this will warning us if we accidentally mutate Redux state(Red de seguridad)
+DevTools Redux: compose - utiles para interactuar con ReduxStore, Asi que las 
+llamadas composeEnhancers aplican Middleware y reduxImmutableStateInvariant es una pieza
+de middleware que estamos usando. Y ahora podemos interactuar con nuestra tienda Redux usando
+utilizando las herramientas de desarrollo de navegador 
+*/
+export default function configureStore(initialState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for Redux dev tools
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+  );
+}
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,13 @@
+/*root reducer, combinaremos todos nuestros reducers para tener un solo reductor
+pasamos un objteo, cada propiedad se establece en el reductor, omite lado derecho courses en objeto.
+Con una exportacion default puedo decidir como nombrar la importacion, courses: afectara la forma
+en que hacemos referencia a esos datos en particular en nuestra tienda redux
+*/
+import { combineReducers } from "redux";
+import courses from "./courseReducer";
+
+const rootReducer = combineReducers({
+  courses
+});
+
+export default rootReducer;
